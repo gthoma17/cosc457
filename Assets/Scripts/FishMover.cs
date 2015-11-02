956363d539eac6b78496bc09f3deb89d9ed975f7
Greg Thomas <gthoma17@emich.edu>
2015-11-02 10:36:23 -0500

    Init commit

---
+++ Assets/Scripts/FishMover.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishMover : MonoBehaviour {
+	public float speed = 1f;
+	public float rightBoundaries = 10f;
+	public float leftBoundaries = -10f;
+	public bool isMovingRight = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isMovingRight){
+			this.transform.position +=  (transform.right * Time.deltaTime) * speed;
+			if(this.transform.position.x > rightBoundaries)
+				Destroy(this.gameObject);
+		}
+		else{
+			this.transform.position -=  (transform.right * Time.deltaTime) * speed;
+			if(this.transform.position.x < leftBoundaries)
+				Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/FishSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishSpawner : MonoBehaviour {
+
+	//public float posX = 0f;
+	//public float posY = 0f;
+	public GameObject fishPrefab;
+	public float fishStartingTime = 1f;
+	public float secondsBetweenFishSpawn = 1f;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("CreateFish",fishStartingTime,secondsBetweenFishSpawn );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void CreateFish(){
+		GameObject fish = Instantiate(fishPrefab) as GameObject;
+		fish.transform.position = transform.position;
+	}
+		
+}
