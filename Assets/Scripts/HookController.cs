3b1d0b4330f6cb0ec59863c1ea6374ff03a0f532
JayyNine <nmerrell@outlook.com>
2015-11-06 21:01:53 -0500

    hook and line continued

--- Assets/Scripts/HookController.cs
+++ Assets/Scripts/HookController.cs
@@ -2,14 +2,23 @@
 using System.Collections;
 
 public class HookController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
 	
+	public float speed;
+	
+	private Rigidbody rb;
+	
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		
+		rb.AddForce (movement * speed);
 	}
 }
