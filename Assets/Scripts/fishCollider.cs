d1c562020523792165fddc917b1f450cee7afe12
Greg Thomas <gthoma17@emich.edu>
2015-11-11 12:57:15 -0500

    rudimentary collisions implemented

---
+++ Assets/Scripts/fishCollider.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fishCollider : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collided: ", gameObject);
+        Destroy(this.gameObject);
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
