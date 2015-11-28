4d4ab00b321f95c20a2f6dc3514e3891fb8552cc
JayyNine <nmerrell@outlook.com>
2015-11-28 14:40:01 -0500

    Forced Aspect Ratio & More
    *Aspect ratio forced on applicable scenes
    *Touch-ups on main menu and how-to pages w/ new aspect ratio
    *Changed Boat
    *Added hook sprite
    
    **Note: catching/collision was broken upon pull

---
+++ Assets/Scripts/AspectRatioController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AspectRatioController : MonoBehaviour {
+	//Source: http://gamedesigntheory.blogspot.ie/2010/09/controlling-aspect-ratio-in-unity.html
+
+	void Start () 
+	{
+		Camera camera = GetComponent<Camera>();
+
+		// force this aspect ratio (16:9)
+		float targetaspect = 16.0f / 9.0f;
+		
+		// determine current aspect ratio and scaling height
+		float windowaspect = (float)Screen.width / (float)Screen.height;
+		float scaleheight = windowaspect / targetaspect;
+
+		// if scaled height is less than current height, add letterbox
+		if (scaleheight < 1.0f)
+		{  
+			Rect rect = camera.rect;
+			
+			rect.width = 1.0f;
+			rect.height = scaleheight;
+			rect.x = 0;
+			rect.y = (1.0f - scaleheight) / 2.0f;
+			
+			camera.rect = rect;
+		}
+		else // add pillarbox
+		{
+			float scalewidth = 1.0f / scaleheight;
+			
+			Rect rect = camera.rect;
+			
+			rect.width = scalewidth;
+			rect.height = 1.0f;
+			rect.x = (1.0f - scalewidth) / 2.0f;
+			rect.y = 0;
+			
+			camera.rect = rect;
+		}
+	}
+}
