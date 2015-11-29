5f6dc19f53e192b545d708ae79005df14f0b7c07
Pegah-Eiz <peizadkh@emich.edu>
2015-11-29 18:42:30 -0500

    created a test row of collectible fishes
    *fish collision is now complete
    *I created a test row of collectible fish to catch on the hook
    *later if the fish that is caught collides with any enemy fishes, it
    dies.

---
+++ Assets/Scripts/enemyCollider.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemyCollider : MonoBehaviour {
+
+
+	void OnTriggerEnter(Collider enemy)
+	{
+		if (enemy.tag.Equals("ceiling"))
+		{
+			screenOverlays.SetWon(true);
+		}
+		else if( enemy.tag.Equals("Fish"))
+		{
+			screenOverlays.SetTrigger(true);
+			Destroy(this.gameObject);
+		}
+		Debug.Log("collided");
+	}
+}
--- Assets/Scripts/fishCollider.cs
+++ Assets/Scripts/fishCollider.cs
@@ -12,21 +12,21 @@ public class fishCollider : MonoBehaviour
         {
             screenOverlays.SetWon(true);
         }
-        /*else if( other.tag.Equals("Fish"))
-        {
-            screenOverlays.SetTrigger(true);
-            Destroy(this.gameObject);
-        }
-        */
+		else if( other.tag.Equals("Fish"))
+		{
+			screenOverlays.SetTrigger(true);
+			Destroy(this.gameObject);
+		}
 		else if(other.tag.Equals("Collectible")){
 			Destroy(other.gameObject);
 			GameObject fish = Instantiate(fishPrefab, this.gameObject.transform.position, this.gameObject.transform.rotation) as GameObject;
+			fish.AddComponent <enemyCollider>();
 			Destroy(this.gameObject);
 			fish.transform.SetParent(HookAndLineParent);
 			//Destroy(other.GetComponent("FishMover"));
 
 		}
-        Debug.Log("collided");    
-    }
+		Debug.Log("collided");    
+	}
     
 }
