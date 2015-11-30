4aaef96c81c184dcc62e08f382116f99ba7a6802
Greg Thomas <gthoma17@emich.edu>
2015-11-30 14:42:28 -0500

    Do better win/lose screen and lives/scores

--- Assets/Scripts/fishCollider.cs
+++ Assets/Scripts/fishCollider.cs
@@ -4,29 +4,50 @@ using System.Collections;
 public class fishCollider : MonoBehaviour
 {
 	public Transform HookAndLineParent;
+    public Transform CaughtFishHolder;
 	public GameObject fishPrefab;
+    public GameObject hookPrefab;
+    public GameObject line;
+    public Vector3 hookStartPos;
+    public Vector3 lineStartPos;
+    public Renderer rend;
+    public GameObject hookedFish = null;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        hookStartPos = this.transform.position;
+        lineStartPos = line.transform.position;
+    }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.tag.Equals("ceiling"))
+        if (other.tag.Equals("hook"))
+        {
+            return;
+        }
+        else if (other.tag.Equals("ceiling") && hookedFish != null)
         {
             screenOverlays.SetWon(true);
         }
-		else if( other.tag.Equals("Fish"))
+        else if ( other.tag.Equals("Fish"))
 		{
-			screenOverlays.SetTrigger(true);
-			Destroy(this.gameObject);
-		}
-		else if(other.tag.Equals("Collectible")){
+            screenOverlays.addLife(-1);
+            rend.enabled = true;
+            line.transform.position = lineStartPos;
+            this.transform.position = hookStartPos;
+            Destroy(other.gameObject);
+            Destroy(hookedFish);
+        }
+		else if(other.tag.Equals("Collectible") && hookedFish == null){
 			Destroy(other.gameObject);
-			GameObject fish = Instantiate(fishPrefab, this.gameObject.transform.position, this.gameObject.transform.rotation) as GameObject;
-			fish.AddComponent <enemyCollider>();
-			Destroy(this.gameObject);
-			fish.transform.SetParent(HookAndLineParent);
-			//Destroy(other.GetComponent("FishMover"));
-
+            GameObject fish = Instantiate(fishPrefab, this.gameObject.transform.position, this.gameObject.transform.rotation) as GameObject;
+            fish.gameObject.tag = "hook";
+            fish.transform.SetParent(HookAndLineParent);
+            hookedFish = fish;
 		}
-		Debug.Log("collided");    
-	}
+        Debug.Log("hook collided with: " + other);
+    }
     
 }
